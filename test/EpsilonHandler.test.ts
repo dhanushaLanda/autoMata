cf8fa579560e125d729f7f09954a18422791a47c
Dhanalakshmi <dhanalakshmilanda@gmail.com>
2019-01-25 23:38:12 +0530

    Add new test case to epsilon handler to check order

--- test/EpsilonHandler.test.ts
+++ test/EpsilonHandler.test.ts
@@ -1,50 +1,64 @@
 import { EpsilonHandler } from "../src/epsilonHandler";
 describe.only("EpsilonHandler",() => {
-  it("handler",() => {
+  it("Should give expected list after appling the epsilon on given input ",() => {
     let configuration = {  
       "q1":{  
         "e":[  
           "q2",
           "q4"
-        ] 
-      },
-      "q2":{  
-        "0":[  
-          "q3"
-        ],
-        "1":[  
-          "q2"
         ]
       },
       "q3":{  
         "0":[  
-          "q2"
-        ],
-        "1":[  
           "q3"
         ]
       },
-      "q4":{ 
+      "q9":{  
         "e":[  
-          "q2",
-        ], 
-        "0":[  
-          "q4"
-        ],
+          "q7"
+        ]
+      },
+      "q7":{  
         "1":[  
-          "q5"
+          "q8"
+        ],
+        "e":[  
+          "q9"
+        ]
+      },
+      "q2":{  
+        "0":[  
+          "q3"
+        ]
+      },
+      "q8":{  
+        "0":[  
+          "q9"
         ]
       },
       "q5":{  
+        "1":[  
+          "q6"
+        ]
+      },
+      "q6":{  
+        "e":[  
+          "q7",
+          "q4"
+        ]
+      },
+      "q4":{  
         "0":[  
           "q5"
         ],
-        "1":[  
-          "q4"
+        "e":[  
+          "q6"
         ]
       }
     };
+    
     let handler = new EpsilonHandler(configuration);
-    expect(handler.handle(["q1"])).toEqual(["q1","q2","q4"]);
+    expect(handler.handle(["q1"])).toEqual(["q1", "q2", "q4", "q6", "q7", "q9"]);
+    expect(handler.handle(["q4"])).toEqual(["q4","q6","q7","q9"]);
   })
 })
